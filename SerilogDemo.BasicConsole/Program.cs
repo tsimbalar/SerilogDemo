52d2c0cd37d4386a85b8e879e6208a28f84c7d78
Thibaud Desodt <tibo.desodt@gmail.com>
2014-12-15 14:27:12 +0100

    first very basic example of level-dependent logging methods

---
+++ SerilogDemo.BasicConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerilogDemo.BasicConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // configuration Serilog, une fois pour toute ...
+            var logger = new LoggerConfiguration()
+                            .WriteTo.ColoredConsole()
+                            .CreateLogger();
+
+            Console.WriteLine("Starting....");
+
+            logger.Information("Doing stuff with the thing");
+            logger.Warning("Be careful, the foo has some baz !");
+
+            logger.Debug("if you are a developper, you may want to know that the answer is 42");
+            logger.Verbose("you would usually not see the verbose output, but who knows ?");
+
+            logger.Error(new InvalidOperationException("bummer! that is a nasty exception"),
+                "Something went very wrong !");
+
+            logger.Fatal("if you see this message, the app is probably dead...");
+
+            Console.WriteLine("Done .... press any key to continue ...");
+            Console.ReadLine();
+            logger.Information("was asked to stop !");
+        }
+    }
+}
---
+++ SerilogDemo.BasicConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SerilogDemo.BasicConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SerilogDemo.BasicConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("774947c7-ab08-408d-85bd-3d8b908f14d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
