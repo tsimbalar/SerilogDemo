3d3c936fa05c3df6222a54af42de855ca2dbc058
Thibaud Desodt <tibo.desodt@gmail.com>
2014-12-15 14:31:36 +0100

    adding logPerClass

--- SerilogDemo.BasicConsole/Program.cs
+++ SerilogDemo.BasicConsole/Program.cs
@@ -11,27 +11,31 @@ namespace SerilogDemo.BasicConsole
     {
         static void Main(string[] args)
         {
-            // configuration Serilog, une fois pour toute ...
-            var logger = new LoggerConfiguration()
-                            .WriteTo.ColoredConsole()
-                            .CreateLogger();
-
             Console.WriteLine("Starting....");
 
-            logger.Information("Doing stuff with the thing");
-            logger.Warning("Be careful, the foo has some baz !");
+            // configuration Serilog, une fois pour toute ...
+            var logConfig = new LoggerConfiguration()
+                            //.MinimumLevel.Debug() // make debug level visible .. default is information
+                            .WriteTo.ColoredConsole();
 
-            logger.Debug("if you are a developper, you may want to know that the answer is 42");
-            logger.Verbose("you would usually not see the verbose output, but who knows ?");
+            // Source context to add information about source in logged events (SourceContext)
+            var classLogger = logConfig.CreateLogger().ForContext<Program>();
 
-            logger.Error(new InvalidOperationException("bummer! that is a nasty exception"),
+
+            classLogger.Information("Doing stuff with the thing");
+            classLogger.Warning("Be careful, the foo has some baz !");
+
+            classLogger.Debug("if you are a developper, you may want to know that the answer is 42");
+            classLogger.Verbose("you would usually not see the verbose output, but who knows ?");
+
+            classLogger.Error(new InvalidOperationException("bummer! that is a nasty exception"),
                 "Something went very wrong !");
 
-            logger.Fatal("if you see this message, the app is probably dead...");
+            classLogger.Fatal("if you see this message, the app is probably dead...");
 
             Console.WriteLine("Done .... press any key to continue ...");
             Console.ReadLine();
-            logger.Information("was asked to stop !");
+            classLogger.Information("was asked to stop !");
         }
     }
 }
